5f89de4c4edc9d579812ee6f32f39bb1a25be7f6
navadeep710 <119839821+navadeep710@users.noreply.github.com>
2022-12-26 11:53:16 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%10==0)     {         printf("%d",n/10);     }     else if(n%5==0)     {         printf("%d",(n/10)+1);     }     else     {         printf("-1");     } }

---
+++ Kohli_and_Coins.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%10==0)
+    {
+        printf("%d",n/10);
+    }
+    else if(n%5==0)
+    {
+        printf("%d",(n/10)+1);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
