0daffc7998e4a2d0837c3d8079da34376f902a56
navadeep710 <119839821+navadeep710@users.noreply.github.com>
2022-12-26 11:44:20 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d",&a,&b);     if((a+b)>10)     {         c=21-(a+b);         printf("%d",c);     }     else if(d!=21 && c>10)     {         printf("-1");     } }

---
+++ Blackjack.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d",&a,&b);
+    if((a+b)>10)
+    {
+        c=21-(a+b);
+        printf("%d",c);
+    }
+    else if(d!=21 && c>10)
+    {
+        printf("-1");
+    }
+}
