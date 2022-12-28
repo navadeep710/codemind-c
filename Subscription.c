f9a371b7b9de7093622fb1c547ed07deff6dda7e
navadeep710 <119839821+navadeep710@users.noreply.github.com>
2022-12-28 23:13:44 +0530

    #include<stdio.h> int main() {     int n,y,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&y)         if(y>30)         {             printf("YES ");         }         else         {             printf("NO ");         }     } }

---
+++ Subscription.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,y,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&y)
+        if(y>30)
+        {
+            printf("YES
+");
+        }
+        else
+        {
+            printf("NO
+");
+        }
+    }
+}
